5618b0b0f89f896fda8224c94b72dc4427509c10
Furkan Abay <furkan_abay@hotmail.com>
2022-10-16 17:33:11 +0200

    Added toggle feature

--- script.js
+++ script.js
@@ -44,9 +44,30 @@ function removeSpinTag() {
 
 /* Adds the "spin" class to "roulette-icon" to make it spin */
 function spinRoulette() {
-    rouletteTable.classList.add("spin");
-    rouletteBtn.disabled = true;
-    rouletteBtn.style.pointerEvents = "none";
-    /* Waits 1 second then executes removeSpinTag */
-    setTimeout(removeSpinTag, 1000);
+    if (!rouletteTable.classList.contains("spin")) {
+        rouletteTable.classList.add("spin");
+        rouletteBtn.disabled = true;
+        rouletteBtn.style.pointerEvents = "none";
+        /* Waits 1 second then executes removeSpinTag */
+        setTimeout(removeSpinTag, 1000);
+    }
 };
+
+/* DARKMODE LIGHTMODE TOGGLE */
+
+const container = document.getElementById("fourht-article");
+const darkModeButton = document.getElementById("darkmode-toggle");
+const sunAndMoon = document.getElementById("sun-and-moon-image");
+
+
+function lightDarkToggle() {
+    if (container.classList.contains("light-mode")) {
+        container.classList.remove("light-mode");
+        container.classList.add("dark-mode")
+        sunAndMoon.src = "/img/sun-and-moon-icons/sun-icon.png";
+    } else if (container.classList.contains("dark-mode")) {
+        container.classList.remove("dark-mode");
+        container.classList.add("light-mode");
+        sunAndMoon.src = "/img/sun-and-moon-icons/moon-icon.png";
+    }
+}
